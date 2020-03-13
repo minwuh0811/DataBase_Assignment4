3f32374fc706af052268ece886baa77e4d9e0af7
minwuh081@gmail.com <minwuh081@gmail.com>
2020-03-13 20:04:06 +0100

    JSON implement and changed SQL Injection

--- src/Repo.java
+++ src/Repo.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
 public interface Repo {
-}
+        void write(String text);
+    }
+
--- src/RepoWrite.java
+++ src/RepoWrite.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.io.Writer;
 
-public class RepoWrite {
+public class RepoWrite implements Repo {
+
+    public RepoWrite(Writer fw) {
+        this.fw = fw;
+    }
+
+    private Writer fw;
+
+    public Writer getFw() {
+        return fw;
+    }
+
+    public void setFw(Writer fw) {
+        this.fw = fw;
+    }
+
+    public void write(String text) {
+        try {
+            fw.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
