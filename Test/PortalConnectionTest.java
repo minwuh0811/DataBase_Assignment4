a7264a9714917c1372ac7f44ccf12a796b169e30
minwuh081@gmail.com <minwuh081@gmail.com>
2020-03-13 20:02:30 +0100

    UnitTest

--- Test/PortalConnectionTest.java
+++ Test/PortalConnectionTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PortalConnectionTest {
+    String path = System.getProperty("user.home")
+            + java.io.File.separator + "IdeaProjects"
+            + java.io.File.separator + "Database"
+            + java.io.File.separator + "Test"
+            + java.io.File.separator + "Resource";
+
 
     @org.junit.jupiter.api.Test
     void register() throws SQLException, ClassNotFoundException {
@@ -18,5 +25,12 @@ class PortalConnectionTest {
     void unregister() throws SQLException, ClassNotFoundException{
         PortalConnection portalConnection=new PortalConnection();
         portalConnection.unregister("1111111111", "CCC111");
+    }
+
+    @Test
+    void writeJSON() throws SQLException, ClassNotFoundException{
+        PortalConnection portalConnection=new PortalConnection();
+        String path_bin = path + java.io.File.separator + "test.json";
+        portalConnection.WriteJSON(path_bin, "2222222222");
     }
 }
